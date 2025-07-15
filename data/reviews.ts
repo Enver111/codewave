239cb53d61beede0d813aab3dbee77cf1542212c
Enver <enver.erfanovich@gmail.com>
2025-07-15 17:09:34 +0300

    Added data for reviews

---
+++ data/reviews.ts
@@ -0,0 +1,22 @@
+export const reviews = [
+  {
+    id: 1,
+    rating: 5,
+    comment: "Отличный сервис! Все сделали быстро и качественно.",
+    user: {
+      id: 1,
+      name: "Иван Иванов",
+      avatar: "/icons/main_logo.png",
+    },
+  },
+  {
+    id: 2,
+    rating: 4,
+    comment: "Хороший сервис, но можно было сделать лучше.",
+    user: {
+      id: 2,
+      name: "Петр Петров",
+      avatar: "/icons/main_logo.png",
+    },
+  },
+];
