901122f5b3e0cf3ff3591ae19126f7d53bf8d1ac
Enver <enver.erfanovich@gmail.com>
2025-08-17 23:54:17 +0300

    Added button component

---
+++ src/UI/Button.tsx
@@ -0,0 +1,15 @@
+export default function Button({
+  children,
+  onClick,
+  className,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+  className?: string;
+}) {
+  return (
+    <button onClick={onClick} className={`${className} `}>
+      {children}
+    </button>
+  );
+}
