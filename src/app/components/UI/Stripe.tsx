dbd7a83e4ce0e4fb0c048e1872b25d2a5e0f0b8b
Enver <enver.erfanovich@gmail.com>
2025-08-17 22:05:05 +0300

    CodeWave 2.0

--- src/app/components/UI/Stripe.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function Stripe() {
-    return (
-        <div className="w-full h-px bg-gradient-to-r from-transparent via-yellow-500/20 to-transparent my-0" />
-    )
-}
--- src/lib/utils.tsx
+++
@@ -1,3 +0,0 @@
-export default function cn(...classes: (string | undefined | false | null)[]) {
-	return classes.filter(Boolean).join(' ');
-}
