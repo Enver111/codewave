5e94efed045cc1a6579836da1a006de2373e862d
Enver <enver.erfanovich@gmail.com>
2025-08-19 00:04:40 +0300

    Added data for achievements and card services

---
+++ data/data.ts
@@ -0,0 +1,58 @@
+export const achievements = [
+  {
+    id: 1,
+    image: "/Achievements/proj.webp",
+    number: "20 +",
+    description: "Проектов",
+    color: "#4584f9",
+  },
+  {
+    id: 2,
+    image: "/Achievements/users.webp",
+    number: "5 000 +",
+    description: "Пользователей",
+    color: "#0dff00",
+  },
+  {
+    id: 3,
+    image: "/Achievements/code.webp",
+    number: "100 000 +",
+    description: "Строк кода",
+    color: "#ff69f4",
+  },
+  {
+    id: 4,
+    image: "/Achievements/design.webp",
+    number: "100 000 +",
+    description: "Дизайн",
+    color: "#f9bb61",
+  },
+];
+
+export const cardsServices = [
+  {
+    id: 1,
+    title: "Web-разработка",
+    description:
+      "Разрабатываем сложные веб-приложения с использованием современных технологий и фреймворков",
+    image: "/Services/dev.webp",
+    color: "rgba(70,175,255,0.22)",
+  },
+  {
+    id: 2,
+    title: "Web-дизайн",
+    description:
+      "Создаем интуитивно понятные и привлекательные интерфейсы, которые повышают конверсию и удовлетворенность пользователей.",
+    image: "/Services/design.webp",
+    color: "rgba(243, 114, 250, 0.22)",
+  },
+
+  {
+    id: 3,
+    title: "Техническая поддержка",
+    description:
+      "Обеспечиваем надежную работу ваших веб-проектов с помощью регулярного обслуживания и оперативной поддержки.",
+    image: "/Services/tech.webp",
+    color: "rgba(255, 248, 85, 0.22)",
+  },
+];
