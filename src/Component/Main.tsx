506692e9b11d285fc7290b78f7a13731c71314ae
Enver <enver.erfanovich@gmail.com>
2025-08-18 23:53:33 +0300

    Added mainbtn component to main page

--- src/Component/Main.tsx
+++ src/Component/Main.tsx
@@ -1,22 +1,27 @@
+import MainBtn from "../UI/MainBtn";
+
 export default function Main() {
   return (
-    <div className="flex items-center justify-center gap-[45px] py-[140px] bg-gradient-to-t from-[#e8f2fc] to-[#e8f2fc00]">
-      <div>
-        <h1 className="text-[64px] font-bold">
-          <span className="bg-gradient-to-r from-[#003aff] via-[#00efff] to-[#003aff] text-transparent bg-clip-text">
-            CodeWave —
-          </span>{" "}
-          <br />
-          мощь веб-решений
-        </h1>
-        <span className="text-[24px]">
-          Создаем современные и <br /> эффективные веб-сайты и приложения,{" "}
-          <br /> которые двигают бизнес вперед.
-        </span>
+    <main className="flex flex-col items-center justify-center bg-gradient-to-t from-[#e8f2fc] to-[#e8f2fc00] py-[140px]">
+      <div className="flex items-center justify-center gap-[45px] mb-[76px]  ">
+        <div>
+          <h1 className="text-[64px] font-bold">
+            <span className="bg-gradient-to-r from-[#003aff] via-[#00efff] to-[#003aff] text-transparent bg-clip-text">
+              CodeWave —
+            </span>{" "}
+            <br />
+            мощь веб-решений
+          </h1>
+          <span className="text-[24px]">
+            Создаем современные и <br /> эффективные веб-сайты и приложения,{" "}
+            <br /> которые двигают бизнес вперед.
+          </span>
+        </div>
+        <div>
+          <img src="/vsCode.svg" alt="vsCode" />
+        </div>
       </div>
-      <div>
-        <img src="/vsCode.svg" alt="vsCode" />
-      </div>
-    </div>
+      <MainBtn>Создать сайт</MainBtn>
+    </main>
   );
 }
